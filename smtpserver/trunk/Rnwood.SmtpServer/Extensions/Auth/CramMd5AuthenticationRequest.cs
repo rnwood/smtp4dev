7bb0c2ee7188c6dda1d53ef51450b549dd555cb3
rnwood_cp <rob@rnwood.co.uk>
2009-08-17 12:26:46 +0000

    Use IServerBehaviour instead of ServerBehaviour and provide simple DefaultServerBehaviour class to facilitate separate hosting of the smtp server. Add authentication checking to IServerBehaviour instead of just blindly accepting everything.

---
+++ smtpserver/trunk/Rnwood.SmtpServer/Extensions/Auth/CramMd5AuthenticationRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rnwood.SmtpServer.Extensions.Auth
+{
+    public class CramMd5AuthenticationRequest : AuthenticationRequest
+    {
+        public CramMd5AuthenticationRequest(string username, string challenge, string challengeResponse)
+        {
+            Username = username;
+            ChallengeResponse = challengeResponse;
+            Challenge = challenge;
+        }
+
+        public string Username { get; private set; }
+
+        public string ChallengeResponse { get; private set; }
+
+        public string Challenge { get; private set; }
+    }
+}
---
+++ smtpserver/trunk/Rnwood.SmtpServer/Extensions/Auth/UsernameAndPasswordAuthenticationRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rnwood.SmtpServer.Extensions.Auth
+{
+    public class UsernameAndPasswordAuthenticationRequest : AuthenticationRequest
+    {
+        public UsernameAndPasswordAuthenticationRequest(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+
+        public string Username { get; private set; }
+        public string Password { get; private set; }
+    }
+}
