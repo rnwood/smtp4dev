3c489154408920e2177f544304f13727a7bc4d07
rnwood_cp <rob@rnwood.co.uk>
2009-09-30 16:26:30 +0000

    Improvements

--- trunk/Rnwood.Smtp4dev/Program.cs
+++ trunk/Rnwood.Smtp4dev/Program.cs
@@ -2,7 +2,9 @@
 
 using System;
 using System.Diagnostics;
+using System.Reflection;
 using System.Windows.Forms;
+using Rnwood.AutoUpdate;
 using Rnwood.Smtp4dev.Properties;
 
 #endregion
@@ -23,7 +25,7 @@ namespace Rnwood.Smtp4dev
             LaunchInfo launchInfo = new LaunchInfo(Environment.CurrentDirectory, args);
             SingleInstanceManager sim =
                 new SingleInstanceManager(string.Format("SMTP4DEV{0}-65D4ED7D-1942-4f39-9F04-66875C67F04A-SESSION{1}",
-                                                        typeof (Program).Assembly.GetName().Version,
+                                                        typeof(Program).Assembly.GetName().Version,
                                                         Process.GetCurrentProcess().SessionId));
             if (sim.IsFirstInstance)
             {
@@ -34,9 +36,18 @@ namespace Rnwood.Smtp4dev
                     Settings.Default.Save();
                 }
 
+                if ((!Settings.Default.LastUpdateCheck.HasValue) || Settings.Default.LastUpdateCheck.Value.AddDays(1) < DateTime.Now)
+                {
+                    Settings.Default.LastUpdateCheck = DateTime.Now;
+                    Settings.Default.Save();
+
+                    UpdateChecker updateChecker = new UpdateChecker(new Uri("file:///c:/temp/releases.xml"), typeof(Program).Assembly.GetName().Version);
+                    updateChecker.CheckForUpdate();
+                }
+
                 MainForm form = new MainForm(launchInfo);
                 sim.LaunchInfoReceived +=
-                    ((s, ea) => form.Invoke((MethodInvoker) (() => form.ProcessLaunchInfo(ea.LaunchInfo, false))));
+                    ((s, ea) => form.Invoke((MethodInvoker)(() => form.ProcessLaunchInfo(ea.LaunchInfo, false))));
                 sim.ListenForLaunches();
 
                 Application.Run(form);
