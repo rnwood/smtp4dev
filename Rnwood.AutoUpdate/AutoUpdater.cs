b88c31e7345528b78c29dce28a159adce9ab549b
rnwood_cp <SND\rnwood_cp>
2009-09-30 06:20:15 +0000

    Auto update framework

---
+++ Rnwood.AutoUpdate/AutoUpdater.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Xml.Serialization;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Rnwood.AutoUpdate
+{
+    public class AutoUpdater
+    {
+        public AutoUpdater(Uri releaseFileUrl, Version currentVersion)
+        {
+            ReleaseFileUrl = releaseFileUrl;
+            CurrentVersion = currentVersion;
+        }
+
+        public Uri ReleaseFileUrl { get; private set; }
+        public Version CurrentVersion { get; private set; }
+
+        public void CheckForUpdate()
+        {
+            Release latestRelease = GetLatestRelease(false);
+            if (latestRelease.Version > CurrentVersion)
+            {
+                UpdateAvailableForm form = new UpdateAvailableForm(latestRelease, CurrentVersion);
+                if (form.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    latestRelease.InitiateDownload();
+                    Application.Exit();
+                }
+            }
+
+        }
+
+        public Release GetLatestRelease(bool includePreRelease)
+        {
+            Release[] releases = GetReleases();
+            return releases.FirstOrDefault(r => includePreRelease || r.Status == releaseStatus.release);
+        }
+
+        private Release[] _releases;
+
+        private Release[] GetReleases()
+        {
+            if (_releases == null)
+            {
+                WebClient webClient = new WebClient();
+                using (Stream stream = webClient.OpenRead(ReleaseFileUrl))
+                {
+                    XmlSerializer ser = new XmlSerializer(typeof(Releases));
+                    _releases = ((Releases)ser.Deserialize(stream)).Release.OrderByDescending(r => r.Version).ToArray();
+                }
+            }
+            return _releases;
+        }
+    }
+}
---
+++ Rnwood.AutoUpdate/UpdateAvailableForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace Rnwood.AutoUpdate
+{
+    public partial class UpdateAvailableForm : Form
+    {
+        public UpdateAvailableForm(Release release, Version currentVersion)
+        {
+            InitializeComponent();
+            label1.Text = string.Format(label1.Text, "smtp4dev");
+            Release = release;
+            currentVersion.Text = CurrentVersion.ToString();
+            updateVersion.Text = release.Version.ToString();
+            detailsLink.Text = release.DetailsURL.ToString();
+        }
+
+        public Release Release { get; private set; }
+
+        private void detailsLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(Release.DetailsURL.ToString());
+        }
+    }
+}
