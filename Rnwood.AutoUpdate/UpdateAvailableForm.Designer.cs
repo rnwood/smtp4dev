0d800578dda826a0801c8d8d6a67b25e99a7d10b
rnwood_cp <SND\rnwood_cp>
2009-12-11 23:38:06 +0000

    Improved auto-update functionality and set correct url for update file. Deploy script merges all dependencies into single exe for standalone version. Tweaks to UI slightly. Display port number program is listening on in systray and main form footer. Re-allow more than one instance Get rid of the .eml file shell context menu items (which didn't work on Vista and Win7 anyway due to system file protection).

--- Rnwood.AutoUpdate/UpdateAvailableForm.Designer.cs
+++ Rnwood.AutoUpdate/UpdateAvailableForm.Designer.cs
@@ -164,7 +164,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(114, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "&Don\'t Update Now";
+            this.button1.Text = "Download &later";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
@@ -175,7 +175,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(80, 23);
             this.button2.TabIndex = 1;
-            this.button2.Text = "&Update";
+            this.button2.Text = "&Download";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // label1
