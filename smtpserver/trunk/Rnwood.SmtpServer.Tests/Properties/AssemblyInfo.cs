3b46a462daee6022405e61ac75cbc82179708816
rnwood_cp <rob@rnwood.co.uk>
2009-12-15 22:15:11 +0000

    Fix for bug #3576 and re-release

--- smtpserver/trunk/Rnwood.SmtpServer.Tests/Properties/AssemblyInfo.cs
+++ smtpserver/trunk/Rnwood.SmtpServer.Tests/Properties/AssemblyInfo.cs
@@ -39,5 +39,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.0.3.*")]
-[assembly: AssemblyFileVersion("2.0.3.*")]
+[assembly: AssemblyVersion("2.0.4.*")]
+[assembly: AssemblyFileVersion("2.0.4.*")]
