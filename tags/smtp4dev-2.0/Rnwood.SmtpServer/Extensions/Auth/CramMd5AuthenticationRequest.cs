c0863b8c31497a93603c95a1f92b851d7cf3852c
rnwood_cp <SND\rnwood_cp>
2010-05-12 20:23:14 +0000

    Tag 2.0 release

---
+++ tags/smtp4dev-2.0/Rnwood.SmtpServer/Extensions/Auth/CramMd5AuthenticationRequest.cs
@@ -0,0 +1,18 @@
+﻿namespace Rnwood.SmtpServer.Extensions.Auth
+{
+    public class CramMd5AuthenticationRequest : IAuthenticationRequest
+    {
+        public CramMd5AuthenticationRequest(string username, string challenge, string challengeResponse)
+        {
+            Username = username;
+            ChallengeResponse = challengeResponse;
+            Challenge = challenge;
+        }
+
+        public string Username { get; private set; }
+
+        public string ChallengeResponse { get; private set; }
+
+        public string Challenge { get; private set; }
+    }
+}
