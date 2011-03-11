c9ca142b8123963463436b2f6dfc33e0693d7fa1
rnwood_cp <SND\rnwood_cp>
2011-03-11 19:22:35 +0000

    *UNSTABLE* work in progress

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 
 [assembly: AssemblyCompany("Robert N Wood <rob@rnwood.co.uk>")]
 [assembly: AssemblyProduct("smtp4dev")]
-[assembly: AssemblyCopyright("Copyright © Robert N Wood 2009")]
+[assembly: AssemblyCopyright("Copyright © Robert N Wood 2009-2011")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyVersion("2.1.1")]
+[assembly: AssemblyFileVersion("2.1.1")]
