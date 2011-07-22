401b8bbca0199d94bbf5c0bbc1fe5e2ec16db01a
rnwood_cp <SND\rnwood_cp>
2011-07-22 06:36:22 +0000

    2.0.6 Fixes

--- Rnwood.SmtpServer/Extensions/Auth/LoginMechanism.cs
+++ Rnwood.SmtpServer/Extensions/Auth/LoginMechanism.cs
@@ -98,7 +98,7 @@ namespace Rnwood.SmtpServer.Extensions.Auth
         {
             try
             {
-                return Encoding.ASCII.GetString(Convert.FromBase64String(data));
+                return Encoding.ASCII.GetString(Convert.FromBase64String(data ?? ""));
             }
             catch (FormatException)
             {
