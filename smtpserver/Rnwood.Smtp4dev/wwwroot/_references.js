632a088f3bf24927638fd36d1263a0dfad682f96
Rob Wood <rob@rnwood.co.uk>
2017-11-11 10:04:22 +0000

    Put the UI out of it's misery - will be rethought. Upgrade project format and update dependencies to latest framework/core/standard versions.

--- smtpserver/Rnwood.Smtp4dev/wwwroot/_references.js
+++
@@ -1,4 +0,0 @@
-﻿/// <autosync enabled="false" />
-/// <reference path="../gulpfile.js" />
-/// <reference path="lib/requirejs/require.js" data-main="js/site.js" start-page="/Index.html" />
-/// <reference path="js/site.js" />
