3c16de34a54f4b2b6e5fa73f48b4cf1272f3a477
rnwood <rob@rnwood.co.uk>
2020-03-06 15:28:02 +0000

    Add execution of the E2E tests on Linux and OSX.

--- Rnwood.Smtp4dev.Tests/E2E/E2ETests.cs
+++ Rnwood.Smtp4dev.Tests/E2E/E2ETests.cs
@@ -25,206 +25,206 @@ using Xunit.Abstractions;
 
 namespace Rnwood.Smtp4dev.Tests.E2E
 {
-	public class E2ETests
-	{
-		private ITestOutputHelper output;
+    public class E2ETests
+    {
+        private ITestOutputHelper output;
 
-		public E2ETests(ITestOutputHelper output)
-		{
-			this.output = output;
-		}
+        public E2ETests(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
 
-		[Fact]
-		public void CheckMessageIsReceivedAndDisplayed()
-		{
-			RunE2ETest((browser, baseUrl, smtpPortNumber) =>
-			{
+        [Fact]
+        public void CheckMessageIsReceivedAndDisplayed()
+        {
+            RunE2ETest((browser, baseUrl, smtpPortNumber) =>
+            {
 
-				browser.Navigate().GoToUrl(baseUrl);
-				HomePage homePage = new HomePage(browser);
+                browser.Navigate().GoToUrl(baseUrl);
+                HomePage homePage = new HomePage(browser);
 
-				HomePage.MessageListControl messageList = WaitFor(() => homePage.MessageList);
-				Assert.NotNull(messageList);
+                HomePage.MessageListControl messageList = WaitFor(() => homePage.MessageList);
+                Assert.NotNull(messageList);
 
-				string messageSubject = Guid.NewGuid().ToString();
-				using (SmtpClient smtpClient = new SmtpClient())
-				{
-					MimeMessage message = new MimeMessage();
-					message.To.Add(new MailboxAddress("to@to.com"));
-					message.From.Add(new MailboxAddress("from@from.com"));
+                string messageSubject = Guid.NewGuid().ToString();
+                using (SmtpClient smtpClient = new SmtpClient())
+                {
+                    MimeMessage message = new MimeMessage();
+                    message.To.Add(new MailboxAddress("to@to.com"));
+                    message.From.Add(new MailboxAddress("from@from.com"));
 
-					message.Subject = messageSubject;
-					message.Body = new TextPart()
-					{
-						Text = "Body of end to end test"
-					};
+                    message.Subject = messageSubject;
+                    message.Body = new TextPart()
+                    {
+                        Text = "Body of end to end test"
+                    };
 
-					smtpClient.Connect("localhost", smtpPortNumber, false, new CancellationTokenSource(TimeSpan.FromSeconds(10)).Token);
-					smtpClient.Send(message);
-					smtpClient.Disconnect(true, new CancellationTokenSource(TimeSpan.FromSeconds(10)).Token);
-				}
+                    smtpClient.Connect("localhost", smtpPortNumber, false, new CancellationTokenSource(TimeSpan.FromSeconds(10)).Token);
+                    smtpClient.Send(message);
+                    smtpClient.Disconnect(true, new CancellationTokenSource(TimeSpan.FromSeconds(10)).Token);
+                }
 
-				HomePage.Grid.GridRow messageRow = WaitFor(() => messageList.Grid?.Rows?.SingleOrDefault());
-				Assert.NotNull(messageRow);
+                HomePage.Grid.GridRow messageRow = WaitFor(() => messageList.Grid?.Rows?.SingleOrDefault());
+                Assert.NotNull(messageRow);
 
-				Assert.Contains(messageRow.Cells, c => c.Text.Contains(messageSubject));
-			});
-		}
+                Assert.Contains(messageRow.Cells, c => c.Text.Contains(messageSubject));
+            });
+        }
 
-		[Fact]
-		public void CheckUTF8MessageIsReceivedAndDisplayed()
-		{
-			RunE2ETest((browser, baseUrl, smtpPortNumber) =>
-			{
+        [Fact]
+        public void CheckUTF8MessageIsReceivedAndDisplayed()
+        {
+            RunE2ETest((browser, baseUrl, smtpPortNumber) =>
+            {
 
-				browser.Navigate().GoToUrl(baseUrl);
-				HomePage homePage = new HomePage(browser);
+                browser.Navigate().GoToUrl(baseUrl);
+                HomePage homePage = new HomePage(browser);
 
-				HomePage.MessageListControl messageList = WaitFor(() => homePage.MessageList);
-				Assert.NotNull(messageList);
+                HomePage.MessageListControl messageList = WaitFor(() => homePage.MessageList);
+                Assert.NotNull(messageList);
 
-				string messageSubject = Guid.NewGuid().ToString();
-				using (SmtpClient smtpClient = new SmtpClient(){ })
-				{
-					MimeMessage message = new MimeMessage();
-					
-					message.To.Add(new MailboxAddress("ñఛ@example.com"));
-					message.From.Add(new MailboxAddress("ñఛ@example.com"));
+                string messageSubject = Guid.NewGuid().ToString();
+                using (SmtpClient smtpClient = new SmtpClient() { })
+                {
+                    MimeMessage message = new MimeMessage();
 
-					message.Subject = messageSubject;
-					message.Body = new TextPart()
-					{
-						Text = "Body of end to end test"
-					};
+                    message.To.Add(new MailboxAddress("ñఛ@example.com"));
+                    message.From.Add(new MailboxAddress("ñఛ@example.com"));
 
-					smtpClient.Connect("localhost", smtpPortNumber, false, new CancellationTokenSource(TimeSpan.FromSeconds(10)).Token);
-					
-					FormatOptions formatOptions = FormatOptions.Default.Clone();
-					formatOptions.International = true;
-					smtpClient.Send(formatOptions, message);
-					smtpClient.Disconnect(true, new CancellationTokenSource(TimeSpan.FromSeconds(10)).Token);
-				}
+                    message.Subject = messageSubject;
+                    message.Body = new TextPart()
+                    {
+                        Text = "Body of end to end test"
+                    };
 
-				HomePage.Grid.GridRow messageRow = WaitFor(() => messageList.Grid?.Rows?.SingleOrDefault());
-				Assert.NotNull(messageRow);
+                    smtpClient.Connect("localhost", smtpPortNumber, false, new CancellationTokenSource(TimeSpan.FromSeconds(10)).Token);
 
-				Assert.Contains(messageRow.Cells, c => c.Text.Contains("ñఛ@example.com"));
-			});
-		}
+                    FormatOptions formatOptions = FormatOptions.Default.Clone();
+                    formatOptions.International = true;
+                    smtpClient.Send(formatOptions, message);
+                    smtpClient.Disconnect(true, new CancellationTokenSource(TimeSpan.FromSeconds(10)).Token);
+                }
 
-		private T WaitFor<T>(Func<T> findElement) where T : class
-		{
-			T result = null;
+                HomePage.Grid.GridRow messageRow = WaitFor(() => messageList.Grid?.Rows?.SingleOrDefault());
+                Assert.NotNull(messageRow);
 
-			CancellationTokenSource timeout = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+                Assert.Contains(messageRow.Cells, c => c.Text.Contains("ñఛ@example.com"));
+            });
+        }
 
-			while (result == null && !timeout.IsCancellationRequested)
-			{
-				result = findElement();
-				Thread.Sleep(100);
-			}
+        private T WaitFor<T>(Func<T> findElement) where T : class
+        {
+            T result = null;
 
-			Assert.NotNull(result);
+            CancellationTokenSource timeout = new CancellationTokenSource(TimeSpan.FromSeconds(10));
 
-			return result;
-		}
+            while (result == null && !timeout.IsCancellationRequested)
+            {
+                result = findElement();
+                Thread.Sleep(100);
+            }
 
+            Assert.NotNull(result);
 
-		private void RunE2ETest(Action<IWebDriver, Uri, int> test)
-		{
-			string workingDir = Environment.GetEnvironmentVariable("SMTP4DEV_E2E_WORKINGDIR");
-			string mainModule = Environment.GetEnvironmentVariable("SMTP4DEV_E2E_BINARY");
-
-			if (string.IsNullOrEmpty(workingDir))
-			{
-				workingDir = Path.GetFullPath("../../../../Rnwood.Smtp4dev");
-			}
-
-			if (string.IsNullOrEmpty(mainModule))
-			{
-				mainModule = Path.GetFullPath("../../../../Rnwood.Smtp4dev/bin/Debug/netcoreapp3.1/Rnwood.Smtp4dev.dll");
-			}
-
-			CancellationToken timeout = new CancellationTokenSource(TimeSpan.FromSeconds(60)).Token;
-			Thread outputThread = null;
-
-			using (Command serverProcess = Command.Run("dotnet", new[] { mainModule, "--urls=http://*:0", "--smtpport=0", "--db=" }, o => o.DisposeOnExit(false).WorkingDirectory(workingDir).CancellationToken(timeout)))
-			{
-				try
-				{
-					IEnumerator<string> serverOutput = serverProcess.GetOutputAndErrorLines().GetEnumerator();
-
-					Uri baseUrl = null;
-					int? smtpPortNumber = 0;
+            return result;
+        }
 
 
-					while ((baseUrl == null || !smtpPortNumber.HasValue) && serverOutput.MoveNext())
-					{
-						string newLine = serverOutput.Current;
-						output.WriteLine(newLine);
+        private void RunE2ETest(Action<IWebDriver, Uri, int> test)
+        {
+            string workingDir = Environment.GetEnvironmentVariable("SMTP4DEV_E2E_WORKINGDIR");
+            string mainModule = Environment.GetEnvironmentVariable("SMTP4DEV_E2E_BINARY");
 
-						if (newLine.StartsWith("Now listening on: http://"))
-						{
-							int portNumber = int.Parse(Regex.Replace(newLine, @".*http://[^\s]+:(\d+)", "$1"));
-							baseUrl = new Uri("http://localhost:" + portNumber);
-						}
+            if (string.IsNullOrEmpty(workingDir))
+            {
+                workingDir = Path.GetFullPath("../../../../Rnwood.Smtp4dev");
+            }
 
-						if (newLine.StartsWith("SMTP Server is listening on port"))
-						{
-							smtpPortNumber = int.Parse(Regex.Replace(newLine, @"SMTP Server is listening on port (\d+).*", "$1"));
-						}
-					}
+            if (string.IsNullOrEmpty(mainModule))
+            {
+                mainModule = Path.GetFullPath("../../../../Rnwood.Smtp4dev/bin/Debug/netcoreapp3.1/Rnwood.Smtp4dev.dll");
+            }
 
-					Assert.False(serverProcess.Process.HasExited, "Server process failed");
+            CancellationToken timeout = new CancellationTokenSource(TimeSpan.FromSeconds(60)).Token;
+            Thread outputThread = null;
 
-					outputThread = new Thread(() =>
-					{
-						while (serverOutput.MoveNext())
-						{
-							string newLine = serverOutput.Current;
-							output.WriteLine(newLine);
-						}
-					});
-					outputThread.Start();
+            using (Command serverProcess = Command.Run("dotnet", new[] { mainModule, "--urls=http://*:0", "--smtpport=0", "--db=" }, o => o.DisposeOnExit(false).WorkingDirectory(workingDir).CancellationToken(timeout)))
+            {
+                try
+                {
+                    IEnumerator<string> serverOutput = serverProcess.GetOutputAndErrorLines().GetEnumerator();
+
+                    Uri baseUrl = null;
+                    int? smtpPortNumber = 0;
 
 
-					ChromeOptions chromeOptions = new ChromeOptions();
-					if (!Debugger.IsAttached)
-					{
-						chromeOptions.AddArgument("--headless");
-					}
+                    while ((baseUrl == null || !smtpPortNumber.HasValue) && serverOutput.MoveNext())
+                    {
+                        string newLine = serverOutput.Current;
+                        output.WriteLine(newLine);
 
-					using (IWebDriver browser = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), chromeOptions))
-					{
-						try
-						{
-							test(browser, baseUrl, smtpPortNumber.Value);
+                        if (newLine.StartsWith("Now listening on: http://"))
+                        {
+                            int portNumber = int.Parse(Regex.Replace(newLine, @".*http://[^\s]+:(\d+)", "$1"));
+                            baseUrl = new Uri("http://localhost:" + portNumber);
+                        }
 
-						}
-						finally
-						{
-							browser.Quit();
-						}
-					}
+                        if (newLine.StartsWith("SMTP Server is listening on port"))
+                        {
+                            smtpPortNumber = int.Parse(Regex.Replace(newLine, @"SMTP Server is listening on port (\d+).*", "$1"));
+                        }
+                    }
 
-				}
-				finally
-				{
-					serverProcess.Kill();
+                    Assert.False(serverProcess.Process.HasExited, "Server process failed");
 
-					if (outputThread != null)
-					{
-						if (!outputThread.Join(TimeSpan.FromSeconds(5)))
-						{
-							outputThread.Abort();
-						}
-				}
+                    outputThread = new Thread(() =>
+                    {
+                        while (serverOutput.MoveNext())
+                        {
+                            string newLine = serverOutput.Current;
+                            output.WriteLine(newLine);
+                        }
+                    });
+                    outputThread.Start();
 
-				
-			}
-		}
-	}
 
-}
+                    ChromeOptions chromeOptions = new ChromeOptions();
+                    if (!Debugger.IsAttached)
+                    {
+                        chromeOptions.AddArgument("--headless");
+                    }
+
+                    using (ChromeDriver browser = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), chromeOptions))
+                    {
+                        try
+                        {
+                            test(browser, baseUrl, smtpPortNumber.Value);
+
+                        }
+                        finally
+                        {
+                            browser.Quit();
+                        }
+                    }
+
+                }
+                finally
+                {
+                    serverProcess.Kill();
+
+                    if (outputThread != null)
+                    {
+                        if (!outputThread.Join(TimeSpan.FromSeconds(5)))
+                        {
+                            outputThread.Abort();
+                        }
+                    }
+
+
+                }
+            }
+        }
+
+    }
 }
 
