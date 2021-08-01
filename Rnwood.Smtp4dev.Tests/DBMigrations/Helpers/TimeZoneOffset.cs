3856192a144ce639a6fae0d5edf55e0cf3c44abc
Jason Finch <jason.finch@gmail.com>
2021-08-01 21:26:29 +1000

    fix (Test/Timezone): Work around issue with TimezoneById on non windows platform.

--- Rnwood.Smtp4dev.Tests/DBMigrations/Helpers/TimeZoneOffset.cs
+++ Rnwood.Smtp4dev.Tests/DBMigrations/Helpers/TimeZoneOffset.cs
@@ -3,6 +3,7 @@
     public class TimeZoneOffset
     {
         public string Id { get; set; }
+        public string SerializedTimeZone { get; set; }
         public int Offset { get; set; }
     }
 }
--- Rnwood.Smtp4dev.Tests/DBMigrations/TimezoneConvertMigrationTests.cs
+++ Rnwood.Smtp4dev.Tests/DBMigrations/TimezoneConvertMigrationTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Runtime.InteropServices;
 using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
 using Rnwood.Smtp4dev.Data;
@@ -25,7 +26,7 @@ namespace Rnwood.Smtp4dev.Tests.DBMigrations
             var timeZone = GetTestTimezone();
             var testDate = new DateTime(2021, 1, 4, 10, 0, 0, DateTimeKind.Utc);
 
-            using (new FakeLocalTimeZone(TimeZoneInfo.FindSystemTimeZoneById(timeZone.Id)))
+            using (new FakeLocalTimeZone(GetTimeZoneInfo(timeZone)))
             {
                 using (var context = new Smtp4devDbContext(_sqlLiteForTesting.ContextOptions))
                 {
@@ -84,7 +85,19 @@ SET ReceivedDate = DATETIME('{testDate:yyyy-MM-dd HH:mm:ss}');
 
         private TimeZoneOffset GetTestTimezone()
         {
-            return new TimeZoneOffset {Id = "Central America Standard Time", Offset = -6};
+            return new TimeZoneOffset
+            {
+                Id = "Central America Standard Time", Offset = -6, SerializedTimeZone =
+                    "Central America Standard Time;-360;(UTC-06:00) Central America;Central America Standard Time;Central America Summer Time;;"
+            };
+        }
+
+        private TimeZoneInfo GetTimeZoneInfo(TimeZoneOffset timeZoneOffset)
+        {
+            // See issue with crossPlatform TimeZoneById calls https://stackoverflow.com/questions/41566395/timezoneinfo-in-net-core-when-hosting-on-unix-nginx
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                return TimeZoneInfo.FindSystemTimeZoneById(timeZoneOffset.Id);
+            return TimeZoneInfo.FromSerializedString(timeZoneOffset.SerializedTimeZone);
         }
     }
 }
