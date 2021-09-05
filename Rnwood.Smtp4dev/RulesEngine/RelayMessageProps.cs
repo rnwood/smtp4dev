c2dc376699443c9f69a4432a6330a99d9b88980c
Jason Finch <jason.finch@gmail.com>
2021-09-06 08:04:54 +1000

    tidy (RelayRules): Document properties.

--- Rnwood.Smtp4dev/RulesEngine/RelayMessageProps.cs
+++ Rnwood.Smtp4dev/RulesEngine/RelayMessageProps.cs
@@ -2,10 +2,22 @@
 
 namespace Rnwood.Smtp4dev.RulesEngine
 {
+    /// <summary>
+    /// Properties that configure additional relay settings when rule is successful
+    /// </summary>
     public class RelayMessageProps
     {
-        public List<string> RelayAdditionalAddress { get; set; }
+        /// <summary>
+        /// List of additional email addresses to dispatch relay messaged to
+        /// </summary>
+        public List<string> RelayAdditionalAddress { get; set; } = new List<string>();
+        /// <summary>
+        /// Should the relay forward to the original recipient?
+        /// </summary>
         public bool RelayToOriginalAddress { get; set; }
+        /// <summary>
+        /// Relay to additional recipients listed in RelayAdditionalAddress
+        /// </summary>
         public bool RelayToAdditionalAddress { get; set; }
     }
 }
