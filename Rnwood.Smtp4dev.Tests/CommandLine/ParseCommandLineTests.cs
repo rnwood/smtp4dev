3fd41dbc4913d3b900804044baacefb82d677ae8
Jason Finch <jason.finch@gmail.com>
2021-09-09 22:24:46 +1000

    fix (CommandLine): RelaySmtpPort now gets set correctly from command line.   Fixes #871

---
+++ Rnwood.Smtp4dev.Tests/CommandLine/ParseCommandLineTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using CommandLiners;
+using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using Xunit;
+
+namespace Rnwood.Smtp4dev.Tests.CommandLine
+{
+    public class ParseCommandLineTests
+    {
+        private const string RelaySmtpPortParam = "--relaysmtpport";
+
+        [Theory]
+        [InlineData(500)]
+        public void CanParseRelaySmtpPort(int portNumber)
+        {
+            var commandLineOptions = CommandLineParser.TryParseCommandLine(new[] { $"{RelaySmtpPortParam}={portNumber}" });
+            var cmdLineOptions = new CommandLineOptions();
+            new ConfigurationBuilder().AddCommandLineOptions(commandLineOptions).Build().Bind(cmdLineOptions);
+            cmdLineOptions.RelayOptions.SmtpPort.Should().Be(portNumber);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(67000)]
+        public void SmtpPortMustBeValidTcpPort(int portNumber)
+        {
+            // TCP port range 1-65535 is valid
+            var commandLineOptions = CommandLineParser.TryParseCommandLine(new[] { $"{RelaySmtpPortParam}={portNumber}" });
+            var cmdLineOptions = new CommandLineOptions();
+            Action act = () => new ConfigurationBuilder().AddCommandLineOptions(commandLineOptions).Build().Bind(cmdLineOptions);
+            act.Should().Throw<TargetInvocationException>().WithInnerException<ArgumentOutOfRangeException>();
+        }
+    }
+}
