4fe8b8085e2ce02ab7f6d77be0affffed0e7a1d0
Jason Finch <jason.finch@gmail.com>
2021-10-04 12:12:19 +1000

    fix (ResourceHelper): Make direcorySeparator cross platform.

--- Rnwood.Smtp4dev.Tests/Resources/ResourceHelper.cs
+++ Rnwood.Smtp4dev.Tests/Resources/ResourceHelper.cs
@@ -8,7 +8,7 @@ namespace Rnwood.Smtp4dev.Tests.Resources
         public static string GetResourcePath(string resourceName)
         {
             var path = AppDomain.CurrentDomain.BaseDirectory;
-            return Path.Join(path, $"Resources\\{resourceName}");
+            return Path.Join(path, $"Resources{Path.DirectorySeparatorChar}{resourceName}");
         }
     }
 }
