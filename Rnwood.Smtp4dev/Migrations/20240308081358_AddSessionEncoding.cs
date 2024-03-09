cc6c2a775ff494e2726ef8ab17bb138b54cc2798
Rob Wood <rob@rnwood.co.uk>
2024-03-09 11:36:09 +0000

    Add missing migrations

---
+++ Rnwood.Smtp4dev/Migrations/20240308081358_AddSessionEncoding.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Rnwood.Smtp4dev.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSessionEncoding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SessionEncoding",
+                table: "Messages",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SessionEncoding",
+                table: "Messages");
+        }
+    }
+}
---
+++ Rnwood.Smtp4dev/Migrations/20240309084542_AddMessageEightBitTransport.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Rnwood.Smtp4dev.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMessageEightBitTransport : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "EightBitTransport",
+                table: "Messages",
+                type: "INTEGER",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EightBitTransport",
+                table: "Messages");
+        }
+    }
+}
