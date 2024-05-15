5e577a8b4152ceb7276a5b8d4432430c9ff9055d
Rob Wood <rob@rnwood.co.uk>
2024-05-15 20:51:41 +0100

    Fix(general): Fix FK error when deleting a session for which a message still exists Resolves #1480. (#1481)

---
+++ Rnwood.Smtp4dev/Migrations/20240515191815_FixSessionMessageCascasing.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Rnwood.Smtp4dev.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixSessionMessageCascasing : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_Sessions_SessionId",
+                table: "Messages");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_Sessions_SessionId",
+                table: "Messages",
+                column: "SessionId",
+                principalTable: "Sessions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_Sessions_SessionId",
+                table: "Messages");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_Sessions_SessionId",
+                table: "Messages",
+                column: "SessionId",
+                principalTable: "Sessions",
+                principalColumn: "Id");
+        }
+    }
+}
