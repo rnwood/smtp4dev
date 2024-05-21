1e5e4330b9cb6cc8a050afff5056cb582248fd5e
Rob Wood <rob@rnwood.co.uk>
2024-05-21 12:30:25 +0000

    chore(dev): Dev container and VS code config improvements

---
+++ .devcontainer/installcustom.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+sudo apt update
+sudo apt install -y telnet
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install -y ./google-chrome-stable_current_amd64.deb
