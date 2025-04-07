62041acba840d03bc98a3f773c6ce24701e5d68c
Rob Wood <rob@rnwood.co.uk>
2025-04-07 06:52:57 +0100

    Cert

--- Rnwood.Smtp4dev/Server/CertificateHelper.cs
+++ Rnwood.Smtp4dev/Server/CertificateHelper.cs
@@ -15,9 +15,8 @@ namespace Rnwood.Smtp4dev.Server
         public static X509Certificate2 LoadCertificateWithKey(string certificatePath, string certificateKeyPath, string password)
         {
             using var rsa = RSA.Create();
-            var keyPem = File.ReadAllText(certificateKeyPath);
-            var keyDer = CertificateHelper.UnPem(keyPem);
-            rsa.ImportPkcs8PrivateKey(keyDer, out _);
+
+            rsa.ImportFromPem(File.ReadAllText(certificateKeyPath));
             var certNoKey = new X509Certificate2(certificatePath);
             var pfxData = certNoKey.CopyWithPrivateKey(rsa).Export(X509ContentType
                             .Pfx);
@@ -122,28 +121,6 @@ namespace Rnwood.Smtp4dev.Server
 
                 return new X509Certificate2(certificatePath, password);
             }
-        }
-
-        /// <summary>
-        /// This is a shortcut that assumes valid PEM
-        /// -----BEGIN words-----\r\nbase64\r\n-----END words-----
-        /// </summary>
-        /// <param name="pem"></param>
-        /// <returns></returns>
-        public static byte[] UnPem(string pem)
-        {
-            const string dashes = "-----";
-            const string newLine = "\r\n";
-            pem = NormalizeLineEndings(pem);
-            var index0 = pem.IndexOf(dashes, StringComparison.Ordinal);
-            var index1 = pem.IndexOf(newLine, index0 + dashes.Length, StringComparison.Ordinal) + newLine.Length;
-            var index2 = pem.IndexOf(dashes, index1, StringComparison.Ordinal) - newLine.Length; //TODO: /n
-            return Convert.FromBase64String(pem.Substring(index1, index2 - index1));
-        }
-
-        private static string NormalizeLineEndings(string val)
-        {
-            return Regex.Replace(val, @"\r\n|\n\r|\n|\r", "\r\n");
         }
     }
 }
