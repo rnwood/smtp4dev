63ea6af23100844112706ff80171375095b85851
Rob Wood <rob@rnwood.co.uk>
2025-04-08 07:30:14 +0100

    Bugs(smtp): Cert load error if cert isn't using RSA - Add DSA and ECDSA (#1687)

--- sendtestemail.ps1
+++ sendtestemail.ps1
@@ -1,14 +1,10 @@
 param($count=1)
 
 
-
+$ErrorActionPreference="Stop"
+write-host "Here"
 
 1..$count | %{ 
-
-    Start-Job -ScriptBlock {
-        param($i)
-
-        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 [System.Net.ServicePointManager]::ServerCertificateValidationCallback = {
     param($source, $cert, $chain, $errors)
@@ -22,5 +18,4 @@ param($count=1)
     <p>Sed pellentesque sit amet nibh eget pellentesque. Etiam mi lectus, elementum vitae sapien id, placerat ornare nunc. Integer sagittis, est id viverra venenatis, sapien velit pulvinar felis, in fringilla urna lorem a libero. Fusce diam magna, laoreet elementum ullamcorper non, fringilla scelerisque dolor. Aenean venenatis arcu vel orci imperdiet, eu semper sapien egestas. Pellentesque condimentum elit in congue vulputate. Phasellus sed lacus feugiat, egestas justo et, sodales lorem. Morbi fringilla bibendum ligula vel imperdiet. Sed non cursus urna, eu consectetur urna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus dui leo, pretium eu commodo eget, sagittis vitae odio. Aenean non arcu imperdiet, malesuada massa at, porta turpis. Proin iaculis ullamcorper imperdiet. Donec lacus lacus, varius eu mauris et, eleifend convallis nisi. In ut nunc tellus. Aliquam convallis congue metus at imperdiet.
     
     <p>Integer ut ligula eu nibh hendrerit aliquam ut nec metus. Nam dignissim lacus nec augue posuere sodales. Praesent suscipit lorem vitae egestas vulputate. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nullam non nibh eget nisi dapibus interdum. Suspendisse eleifend felis quis leo tempus, eget ultricies magna cursus. Vivamus ullamcorper bibendum urna a eleifend. Donec ac faucibus massa. Maecenas vehicula, urna id commodo tincidunt, mauris velit placerat orci, ut porta nisi quam id dolor. In blandit lacus sed lacus mollis, eget iaculis quam posuere. Vivamus tristique urna quis sem faucibus aliquam. Nam scelerisque egestas risus ullamcorper consequat. Vestibulum condimentum volutpat est, vitae lobortis diam dapibus eu. Proin aliquam purus sit amet diam facilisis suscipit. Vestibulum in ipsum nunc. "
-} | receive-job
-}
+} 
